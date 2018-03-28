13bea385dbf72bf43397cf794e83629c08a38dfc
Lucivaldo <lucious@hpc4480.local>
2018-03-28 11:00:51 -0300

    Adding more IT testes to CuisineController

--- src/test/java/br/com/skip/lucious/controller/CuisineControllerIT.java
+++ src/test/java/br/com/skip/lucious/controller/CuisineControllerIT.java
@@ -2,13 +2,17 @@ package br.com.skip.lucious.controller;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+import static org.hamcrest.core.StringContains.containsString;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.json.JSONException;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
@@ -16,7 +20,7 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 
 import br.com.skip.lucious.AbstractIntegrationTest;
 
-@DatabaseSetup("classpath:fixtures/cuisine.xml")
+@DatabaseSetup({"classpath:fixtures/cuisine.xml"})
 public class CuisineControllerIT extends AbstractIntegrationTest {
 
 
@@ -35,7 +39,7 @@ public class CuisineControllerIT extends AbstractIntegrationTest {
 
 	@Test
 	@SuppressWarnings("rawtypes")
-	public void testRetrieveAll() throws JSONException {
+	public void testRetrieveAll() {
 
 		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
 
@@ -43,6 +47,51 @@ public class CuisineControllerIT extends AbstractIntegrationTest {
 				entity, List.class);
 
 		assertThat(response.getBody().size(), is(4));
+	}
+	
+	@Test
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void testSearch() {
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<List> response = restTemplate.exchange(createURL("/Cuisine/search/nese"), HttpMethod.GET,
+				entity, List.class);
+
+		List<String> body = (List<String>) response.getBody().stream().map((r) -> r.toString()).collect(Collectors.toList());
+
+		assertThat(body.size(), is(2));
+		assertThat(body, hasItems(containsString("Japanese"), containsString("Chinese")));
+	}
+	
+	@Test
+	@DatabaseSetup("classpath:fixtures/store.xml")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void testRetriveByCuisine() {
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<List> response = restTemplate.exchange(createURL("/Cuisine/3/stores"), HttpMethod.GET,
+				entity, List.class);
+
+		List<String> body = (List<String>) response.getBody().stream().map((r) -> r.toString()).collect(Collectors.toList());
+
+		assertThat(body.size(), is(2));
+		assertThat(body, hasItems(containsString("Mr Miyagi"), containsString("The Great Sushi")));
+	}
+	
+	@Test
+	public void testCreateNew() throws JSONException {
+		
+		String posted = "{'name': 'Neo Cuisine'}".replace('\'', '"');
+		headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+		
+		HttpEntity<String> entity = new HttpEntity<String>(posted, headers);
+		
+		ResponseEntity<String> response = restTemplate.exchange(createURL("/Cuisine/"), HttpMethod.POST,
+				entity, String.class);
+		
+		String expected = "{id: 5, name:'Neo Cuisine'}";
+
+		JSONAssert.assertEquals(expected, response.getBody(), false);
 	}
 
 }
